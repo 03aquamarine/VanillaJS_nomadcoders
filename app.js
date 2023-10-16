ea0a7f78effea23e619cacba289ba74ec498edda
yujinroh <yujinroh37@gmail.com>
2023-10-16 13:55:50 +0900

    feat: #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
-  event.preventDefault(); // 어떤 event의 기본 행동이든지 발생하지 않도록 막음
+  event.preventDefault();
   console.log(loginInput.value);
 }
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
