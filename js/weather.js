e05f07afcac76fe75c62eb4e1de37796588ec535
yujinroh <yujinroh37@gmail.com>
2023-10-17 17:32:14 +0900

    feat: #8.0~1 Geolocation, Weather API

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "3de02e0f216b42ec8851cb29c29e5a88";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
